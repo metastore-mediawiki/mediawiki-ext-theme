84dc5d41f6265e633006e32063cb100ae44c1703
Kitsune Solar <kitsune.solar@gmail.com>
2018-07-20 15:51:55 +0300

    2018-07-20 12:51:55

--- MW_EXT_Theme.class.php
+++ MW_EXT_Theme.class.php
@@ -18,10 +18,10 @@ class MW_EXT_Theme {
 	 * @return bool
 	 */
 	public static function onBeforePageDisplay( OutputPage $out, Skin $skin ) {
-		$out->addStyle( 'https://fonts.googleapis.com/css?family=Roboto+Condensed:400,400i,700,700i|Roboto:400,400i,700,700i|Fira+Mono:400,700&amp;subset=cyrillic', 'screen' );
+		$out->addStyle( 'https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,300i,700|Open+Sans:400,400i,700,700i|Fira+Mono:400,700&amp;subset=cyrillic', 'screen' );
 		$out->addStyle( 'https://use.fontawesome.com/releases/v5.1.1/css/all.css', 'screen' );
-		$out->addStyle( '/extensions/MW_EXT_Meta/modules/styles/theme.css', 'screen' );
-		$out->addModules( [ 'ext.mw.meta' ] );
+		$out->addStyle( '/extensions/MW_EXT_Theme/modules/styles/theme.css', 'screen' );
+		$out->addModules( [ 'ext.mw.theme' ] );
 
 		return true;
 	}
